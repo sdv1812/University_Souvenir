a7459ac73095b9d5b9fc7260394dc4cfc476cadc
koushikcr <c.r.koushik@gmail.com>
2016-04-02 10:02:27 +0530

    JUnit Transaction commit

--- src/sg/edu/nus/iss/store/Store.java
+++ src/sg/edu/nus/iss/store/Store.java
@@ -8,19 +8,18 @@ import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 
-
 public class Store {
 	private MemberRegister members;
 	private CategoryRegister categories;
 	private StoreKeeperRegister storeKeepers;
 	private DiscountManager discounts;
 	private VendorRegister vendors;
-	private ProductRegister products; //xuemin
+	private ProductRegister products; // xuemin
 	private Cart cart;
 	private ArrayList<Product> product;
 	private AddProductToCart addProductsToCart;
-	private Transaction transaction ;
-	private ArrayList<Cart> cartList ;
+	private Transaction transaction;
+	private ArrayList<Cart> cartList;
 	private ArrayList<Transaction> transactionList;
 
 	public Store() {
@@ -33,58 +32,59 @@ public class Store {
 		transaction = new Transaction();
 		transactionList = new ArrayList<Transaction>();
 		addProductsToCart = new AddProductToCart();
-		products=new ProductRegister(this); //xuemin
+		products = new ProductRegister(this); // xuemin
 	}
-	public ProductRegister getProductReg(){ //xuemin
+
+	public ProductRegister getProductReg() { // xuemin
 		return products;
 	}
 
-	public ArrayList<Product> getProducts()
-	{
+	public ArrayList<Product> getProducts() {
 		return products.getProducts();
 	}
-	//add
-	public ArrayList<Product> getProductsBelowThreshold(){
+
+	// add
+	public ArrayList<Product> getProductsBelowThreshold() {
 		return products.checkProductsBelowThreshold();
 	}
-	
 
-	public CategoryRegister getCategoryReg(){ //xuemin
+	public CategoryRegister getCategoryReg() { // xuemin
 		return categories;
 	}
-	/*public void readDataFromFile() throws IOException{ //xuemin
-		products.createListFromFile();
-	}*/
+	/*
+	 * public void readDataFromFile() throws IOException{ //xuemin
+	 * products.createListFromFile(); }
+	 */
 
-	public void writeDataToFile() throws IOException{ //xuemin
+	public void writeDataToFile() throws IOException { // xuemin
 		products.writeListToFile();
 	}
 
-	public void removeProduct(Product p) throws IOException{  //xuemin
+	public void removeProduct(Product p) throws IOException { // xuemin
 		products.removeProduct(p);
 	}
 
-	public void removeProduct(String id) throws IOException{  //xuemin
+	public void removeProduct(String id) throws IOException { // xuemin
 		products.removeProduct(id);
 	}
 
-	public void addStoreKeeper(String storeKeeperName, String storeKeeperPassword){
+	public void addStoreKeeper(String storeKeeperName, String storeKeeperPassword) {
 		storeKeepers.addStoreKeeper(storeKeeperName, storeKeeperPassword);
 		storeKeepers.writeToFile();
 	}
 
-	public boolean validate(String storeKeeperName, String password){
+	public boolean validate(String storeKeeperName, String password) {
 		return storeKeepers.validate(storeKeeperName, password);
 	}
 
-	public boolean addMember(String memberName, String memberID){
+	public boolean addMember(String memberName, String memberID) {
 		boolean b = members.addMember(memberName, memberID);
 		members.writeToFile();
 		return b;
 	}
 
 	public boolean addCategory(String categoryCode, String categoryName) {
-		boolean b= categories.addCategory(categoryCode, categoryName);
+		boolean b = categories.addCategory(categoryCode, categoryName);
 		categories.writeToFile();
 		return b;
 	}
@@ -97,18 +97,19 @@ public class Store {
 		members.removeMember(memberID);
 		members.writeToFile();
 	}
+
 	public void printAllCategories() {
 		ArrayList<Category> temp = categories.getCategories();
-		for(Category c : temp){
-			System.out.println("Category Name : "+c.getCategoryName());
-			System.out.println("Category Code : "+c.getCategoryCode()+"\n");
+		for (Category c : temp) {
+			System.out.println("Category Name : " + c.getCategoryName());
+			System.out.println("Category Code : " + c.getCategoryCode() + "\n");
 		}
 	}
 
 	public void printAllMembers() {
-		ArrayList<Member> temp = new ArrayList<Member> ();
+		ArrayList<Member> temp = new ArrayList<Member>();
 		temp = members.getMembers();
-		for(Member m : temp){
+		for (Member m : temp) {
 			System.out.println(m.getCustomerName());
 			System.out.println(m.getLoyaltyPoints());
 			System.out.println(m.getMemberID());
@@ -120,7 +121,7 @@ public class Store {
 		return members.getMembers();
 	}
 
-	public void initializeData(){
+	public void initializeData() {
 		try {
 			storeKeepers.createListFromFile();
 			members.createListFromFile();
@@ -135,7 +136,6 @@ public class Store {
 			e.printStackTrace();
 		}
 
-
 	}
 
 	public void removeCategory(String categoryCode) {
@@ -149,7 +149,8 @@ public class Store {
 		return discounts.getDiscounts();
 	}
 
-	public boolean addDiscount(String discountCode, String description, float percentage, String startDate, String discountPeriod) {
+	public boolean addDiscount(String discountCode, String description, float percentage, String startDate,
+			String discountPeriod) {
 		boolean b = discounts.addDiscount(discountCode, description, percentage, startDate, discountPeriod);
 		discounts.writeToFile();
 		return b;
@@ -182,21 +183,21 @@ public class Store {
 		return total;
 	}
 
-	public void makePayment(double amountreceived, double transactiontotal,
-			double discountValue, double redeemPointsValue, ArrayList<Cart> cart) {
-		transaction.makePayment(amountreceived, transactiontotal, discountValue, redeemPointsValue, cart, members, products);
+	public void makePayment(double amountreceived, double transactiontotal, double discountValue,
+			double redeemPointsValue, ArrayList<Cart> cart) {
+		transaction.makePayment(amountreceived, transactiontotal, discountValue, redeemPointsValue, cart, members,
+				products);
 		transaction.writeToFile();
 	}
-
 
 	public void removeCartItem(Cart lineItem) {
 		cartList.remove(lineItem);
 	}
 
-	public boolean  addProductsToCart(Product product,int quantity ,Member member){
+	public boolean addProductsToCart(Product product, int quantity, Member member) {
 		boolean addProductStatus = false;
-		Cart c1 =addProductsToCart.addProductsToCart(product, quantity, member);
-		if(c1!=null){
+		Cart c1 = addProductsToCart.addProductsToCart(product, quantity, member);
+		if (c1 != null) {
 			cartList.add(c1);
 			addProductStatus = true;
 		}
@@ -210,69 +211,85 @@ public class Store {
 	public boolean addVendor(String vendorName, String vendorDescription, Category category) {
 		return vendors.addVendor(vendorName, vendorDescription, category);
 	}
+
 	public ArrayList<Vendor> getVendorsPerCategory(Category category) {
 		return vendors.getVendorsPerCategory(category);
 	}
+
 	public void removeVendor(String vendorName) {
 		vendors.removeVendor(vendorName);
 	}
 
-	public ArrayList<Transaction> getTransaction(){
+	public ArrayList<Transaction> getAllTransaction() {
 		ArrayList<Transaction> t1 = transaction.getAllTransactions();
 		transactionList.addAll(t1);
 		return transactionList;
 	}
+
 	public Member getMember(String memberIdentity) {
 		return members.getMember(memberIdentity);
 	}
+
 	public Product getProductByID(String productIdentity) {
 		return products.getProductById(productIdentity);
 	}
+
 	public Category getCategory(String categoryCode) {
 		return categories.getCategory(categoryCode);
 	}
+
 	public Vendor getVendor(String vendorName) {
 		return getVendor(vendorName);
 	}
+
 	public Category getCategoryByName(String categoryName) {
 		// TODO Auto-generated method stub
 		return categories.getCategorybyName(categoryName);
 	}
-	
+
 	public AbstractTableModel getCategoryTableModel() {
 		return categories.getCategoryTableModel();
 	}
-	
+
 	public AbstractTableModel getMemberTableModel() {
 		return members.getMemberTableModel();
 	}
-	
+
 	public AbstractTableModel getProductTableModel() {
 		return products.getProductTableModel();
 	}
-	
+
 	public AbstractTableModel getVendorTableModel() {
 		return vendors.getVendorTableModel();
 	}
-	
+
 	public String getLoyaltyPoints() {
 		int loyaltyPoints = transaction.getLoyaltyPoints();
 		String loyalPoints = Integer.toString(loyaltyPoints);
 		return loyalPoints;
-		}
-	public String beginCheckout(List<Cart> cartProducts) {//CHANGE 27-3
+	}
+
+	public String beginCheckout(List<Cart> cartProducts) {// CHANGE 27-3
 		double discountPerc = discounts.getMaxDiscount(cartProducts.get(0).getMember());
-		String cartStatus = transaction.addProductsToCart(cartProducts,discountPerc);
+		String cartStatus = transaction.addProductsToCart(cartProducts, discountPerc);
 		return cartStatus;
-		}
+	}
+
 	public double getBonusPoints() {
 		return transaction.getBonusPoints();
 	}
+
 	public ArrayList<Transaction> getTransactions(String fromDate, String toDate) throws ParseException {
 		return transaction.getTransactions(fromDate, toDate);
 	}
 
+	public MemberRegister getMemberRegister() {
+		return members;
+	}
 
+	public Transaction getTransaction() {
+		return transaction;
 
+	}
 
 }
